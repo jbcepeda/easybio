000ef800f960dfcedb7e9c75c9cc8a4a3d9bd7c5
Benjamin Cepeda <josebenjamincepeda@gmail.com>
2023-03-07 16:27:48 -0500

    Verificacion put Ubicacion

--- api/views.py
+++ api/views.py
@@ -72,7 +72,7 @@ class GenericObjects(APIView):
     def post(self, request):
         try:
             serializer = self.serializer_class(data = request.data)
-            if serializer.is_valid():
+            if request and len(request.data)>0 and serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status = status.HTTP_201_CREATED)
         except Exception as ex:
